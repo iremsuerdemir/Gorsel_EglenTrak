07610a65b62b14f9ef4b439ed594e95a32e5f3f6
Polat Avcı <polatavci2222@gmail.com>
2025-04-13 22:10:06 +0300

    userService dosyası eklendi

---
+++ lib/services/base_url.dart
@@ -0,0 +1,3 @@
+class BaseUrl {
+  static String baseUrl = "https://localhost:7176/api";
+}
---
+++ lib/services/user_service.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:gorsel_programlama_proje/models/user.dart';
+import 'package:gorsel_programlama_proje/services/base_url.dart';
+import 'package:http/http.dart' as http;
+
+class UserService {
+  static User? _user;
+  static get user => _user;
+
+  static Future<User?> login({
+    required String email,
+    required String password,
+  }) async {
+    final response = await http.post(
+      Uri.parse("${BaseUrl.baseUrl}/Users/Login"),
+      headers: {
+        'Content-Type':
+            'application/json', // JSON formatında gönderildiğini belirt
+      },
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    if (response.statusCode == 200) {
+      _user = User.fromJson(jsonDecode(response.body));
+    } else {
+      _user = null;
+    }
+
+    return _user;
+  }
+
+  static void logout() {
+    _user = null;
+  }
+
+  static Future<User?> register({
+    required String email,
+    required String username,
+    required String password,
+  }) async {
+    final response = await http.post(
+      Uri.parse("${BaseUrl.baseUrl}/Users"),
+      headers: {
+        'Content-Type':
+            'application/json', // JSON formatında gönderildiğini belirt
+      },
+      body: {"email": email, "password": password, "username": username},
+    );
+
+    if (response.statusCode == 201) {
+      _user = User.fromJson(jsonDecode(response.body));
+    } else {
+      _user = null;
+    }
+    return _user;
+  }
+}
