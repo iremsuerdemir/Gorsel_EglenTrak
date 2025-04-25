827743f37f3868e19d64017b0da19ff064a83a46
iremsuerdemir <7692003@gmail.com>
2025-04-25 18:35:56 +0300

    Revert ":wqMerge branch 'bilgiYarismasi'"
    This reverts commit 0eeb9acb87e604362f138199aa31115ddd4106ac, reversing
    changes made to f5bafddba54f84e2e087732f93e5ca9467aa949f.

--- Gorsel_Programlama_Proje-master/android/app/src/main/kotlin/com/example/gorsel_programlama_proje/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gorsel_programlama_proje
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Gorsel_Programlama_Proje-master/lib/services/base_url.dart
+++
@@ -1,3 +0,0 @@
-class BaseUrl {
-  static String baseUrl = "https://localhost:7176/api";
-}
